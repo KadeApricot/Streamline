358d5b13b0c53551fbf740dfcb50257eb3942ba5
KadeApricot <k4deapricot.std@gmail.com>
2026-02-24 20:28:13 +0700

    flesh out aabb

--- aabb.py
+++ aabb.py
@@ -1,26 +1,31 @@
 import shape as shape
+import transform as transform
 
 class AABB(shape.Shape):
-    def __init__(self, x : float, y : float, w : float, h : float):
-        self.x = x
-        self.y = y
+    def __init__(self, float, w : float, h : float):
         self.w = w
         self.h = h
     
     def get_mtv(self, other : shape.Shape):
         if isinstance(other, AABB):
-            dx = (self.x + self.w / 2) - (other.x + other.w / 2)
-            dy = (self.y + self.h / 2) - (other.y + other.h / 2)
-            combined_half_widths = (self.w + other.w) / 2
-            combined_half_heights = (self.h + other.h) / 2
+            transform_comp = self.entity.get_component(transform.Transform)
+            other_transform_comp = other.entity.get_component(transform.Transform)
+            
+            dx = transform_comp.pos.x - other_transform_comp.pos.x
+            px = (self.w + other.w) / 2 - abs(dx)
+            if px <= 0:
+                return None
 
-            if abs(dx) < combined_half_widths and abs(dy) < combined_half_heights:
-                overlap_x = combined_half_widths - abs(dx)
-                overlap_y = combined_half_heights - abs(dy)
+            dy = transform_comp.pos.y - other_transform_comp.pos.y
+            py = (self.h + other.h) / 2 - abs(dy)
+            if py <= 0:
+                return None
 
-                if overlap_x < overlap_y:
-                    return (overlap_x if dx > 0 else -overlap_x, 0)
-                else:
-                    return (0, overlap_y if dy > 0 else -overlap_y)
-        
+            if px < py:
+                sx = 1 if dx < 0 else -1
+                return (px * sx, 0)
+            else:
+                sy = 1 if dy < 0 else -1
+                return (0, py * sy)
+
         return None
